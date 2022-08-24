0d21925360a59ae21add7ad873152c84ca25d96a
Realsammy <realsammy86@gmail.com>
2022-08-24 09:29:02 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,20 @@
-recognition.stop();
+const recognitionSvc = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new recognitionSvc();
+
+document.querySelector('#start').addEventListener('click', ()=>{
+  const startBtn = document.querySelector('#start');
+  recognition.lang = document.querySelector('#lang').value || 'en-GB';
+  recognition.continuous = true;
+
+  recognition.onresult = (event) => {
+    const accumulatedResult = [];
+    for (const result of event.results) {
+      accumulatedResult.push(`${result[0].transcript}`);
+    }
+    document.querySelector('#transcription').innerHTML = accumulatedResult;
+  };
+  
+  if(startBtn.classList.contains('listening')) recognition.stop();
   else {
     recognition.start();
   }
@@ -16,3 +32,7 @@ document.querySelector('#transcription').addEventListener('click', (e) => {
   });
   
 });
+
+setTimeout( () => {
+  document.getElementById('tooltip').classList.remove('init');
+}, 3100);
